80891a60f945d5a9f4158dbd10689e7857226428
profitmediabusiness <profitmediabusiness@gmail.com>
2023-08-08 22:05:09 +0700

    Apply common layout

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -58,7 +58,11 @@ class TaskController extends Controller
 
     public function index()
     {
+        $pageTitle = 'Task List';
         $tasks = $this->tasks;
-        return view('tasks.index', ['tasks' => $tasks]);
+        return view('tasks.index', [
+            'pageTitle' => $pageTitle,
+            'tasks' => $tasks
+        ]);
     }
 }

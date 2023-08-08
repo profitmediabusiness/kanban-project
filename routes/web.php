c41d0e37c780a2b279c577b344fe22b64097e7d3
profitmediabusiness <profitmediabusiness@gmail.com>
2023-08-08 22:16:07 +0700

    Develop Edit Task View

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ Route::get('/layouts', function () {
 Route::get('/tasks/', [TaskController::class, 'index'])
 ->name('tasks.index'); 
  // 
+ Route::get('/tasks/{id}/edit', [TaskController::class, 
+ 'edit'])->name('tasks.edit');

56ac02d1f7ae5aa9f1938a26a73d6a78671c8165
profitmediabusiness <profitmediabusiness@gmail.com>
2023-09-15 17:53:03 +0700

    Sudah selesai membuat view dari Role And Permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id'
     ];
 
     /**
@@ -42,4 +44,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
